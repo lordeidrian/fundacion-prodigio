a0158455597c12a738acefc9a85acce6752acc63
lordeidrian <adrianurunaga@outlook.com>
2025-08-25 11:30:32 -0300

    .

--- app/Models/SupportMethod.php
+++ app/Models/SupportMethod.php
@@ -4,16 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class SupportMethod extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'title',
         'description',
@@ -23,4 +19,34 @@ class SupportMethod extends Model
         'order',
         'is_active',
     ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'order'     => 'integer',
+    ];
+
+    /**
+     * description: limpia <p> al guardar.
+     */
+    protected function description(): Attribute
+    {
+        return Attribute::make(
+            set: function ($value) {
+                if (!is_string($value)) {
+                    return $value;
+                }
+
+                $value = html_entity_decode(trim($value), ENT_QUOTES | ENT_HTML5, 'UTF-8');
+
+                if (preg_match('/^<p>(.*)<\/p>$/s', $value)) {
+                    $value = preg_replace('/^<p>(.*)<\/p>$/s', '$1', $value);
+                }
+
+                $value = preg_replace('/<\/?p[^>]*>/i', '', $value);
+
+
+                return $value;
+            },
+        );
+    }
 }
