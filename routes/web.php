74a984fbd182a76fb107d883af02da01b3078580
lordeidrian <adrianurunaga@outlook.com>
2026-01-15 17:20:52 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\BlogController;
+use App\Models\Project;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,32 +17,28 @@ use App\Http\Controllers\BlogController;
 // --- Rutas de Páginas Estáticas ---
 Route::get('/', [PageController::class, 'inicio'])->name('inicio');
 Route::get('/nosotros', [PageController::class, 'nosotros'])->name('nosotros');
-
-// --- RUTA FALTANTE AÑADIDA AQUÍ ---
 Route::get('/contacto', [PageController::class, 'contacto'])->name('contacto');
-
-// --- Ruta para el Sitemap (SEO) ---
-Route::get('/sitemap.xml', function () {
-    $projects = \App\Models\Project::where('status', 'published')->get();
-    $posts = \App\Models\Post::where('status', 'published')->get();
-
-    return response()->view('sitemap', compact('projects', 'posts'))
-        ->header('Content-Type', 'application/xml');
-});
-
 
 // --- Ruta para procesar el envío del formulario de contacto ---
 Route::post('/contacto', [PageController::class, 'submitContact'])->name('contact.submit');
 
+// --- Ruta para el Sitemap (SEO) ---
+Route::get('/sitemap.xml', function () {
+    $projects = Project::where('status', 'published')->get();
+    $posts = Post::where('status', 'published')->get();
 
-// --- Rutas para Proyectos ---
+    return response()
+        ->view('sitemap', compact('projects', 'posts'))
+        ->header('Content-Type', 'application/xml');
+})->name('sitemap');
+
+// --- Rutas para Proyectos (URL pública: /nuestro-trabajo) ---
 Route::prefix('nuestro-trabajo')->name('proyectos.')->group(function () {
     Route::get('/', [ProjectController::class, 'index'])->name('index');
     Route::get('/{project:slug}', [ProjectController::class, 'show'])->name('show');
 });
 
-
-// --- Rutas para el Blog (Noticias y Eventos) ---
+// --- Rutas para el Blog (URL pública: /blog) ---
 Route::prefix('blog')->name('noticias.')->group(function () {
     Route::get('/', [BlogController::class, 'index'])->name('index');
     Route::get('/{post:slug}', [BlogController::class, 'show'])->name('single');
